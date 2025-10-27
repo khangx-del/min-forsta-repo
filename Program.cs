799e2aa3a3e882212c9053dd5888d31e16c34133
khangx-del <kn7488909@gmail.com>
2025-10-27 12:28:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input_and_Output_in_C_.pptx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey();
+        }
+    }
+}
